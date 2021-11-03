4a882e46708c6feafee1761fc15b002edbc13301
Szymon Matras <matras1999@gmail.com>
2021-11-03 13:29:36 +0100

    wersja 1.2

--- funkcja.cpp
+++ funkcja.cpp
@@ -8,14 +8,16 @@ int main()
     cout << "Program do obliczania miejsc zerowych funkcji liniowej." << endl;
     cout << "Podaj a:";
     cin >> a;
-    if (a==0)
-    {
-        cout << "a nie moze byc zerem!";
-        return 0;
-    }
     cout << "Podaj b: ";
     cin >> b;
-
+    if (a==0&&b==0)
+    {
+        cout << "Funkcja ma nieskonczenie wiele  miejsc zerowych!" << endl;
+    }
+    if (a==0&&b>0)
+    {
+        cout << "Funkcja nie ma miejsca zerowego!" << endl;
+    }
     msc0=(-b)/a;
     cout << "Miejsce zerowe funkci to: " << msc0;
 
@@ -24,3 +26,4 @@ int main()
 
     return 0;
 }
+
