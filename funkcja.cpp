3fe6c57fda256133a9114f0bbe322e6e716a47b1
Szymon Matras <matras1999@gmail.com>
2021-11-03 13:58:10 +0100

    wersja2.0

--- funkcja.cpp
+++ funkcja.cpp
@@ -1,29 +1,44 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
-    int a , b  ,x,msc0;
-    cout << "Program do obliczania miejsc zerowych funkcji liniowej." << endl;
+    int a , b ,c ,msc1,msc0,delta;
+    cout << "Program do obliczania miejsc zerowych funkcji kwadratowej." << endl;
     cout << "Podaj a:";
     cin >> a;
+    if (a==0)
+    {
+        cout << "a nie moze byc 0!";
+        return 0;
+    }
     cout << "Podaj b: ";
     cin >> b;
-    if (a==0&&b==0)
+    cout << "Podaj c: ";
+    cin >>c;
+    delta = (b*b)-4*a*c;
+    msc0=(-b-sqrt(delta))/2*a;
+    msc1=(-b+sqrt(delta))/2*a;
+    if(delta<0)
     {
-        cout << "Funkcja ma nieskonczenie wiele  miejsc zerowych!" << endl;
+        cout << "Brak miejsc zerowych!";
+        return 0;
     }
-    if (a==0&&b>0)
+    if (delta==0)
     {
-        cout << "Funkcja nie ma miejsca zerowego!" << endl;
+        cout << "Funkca ma jedno miejsce zerowe jakim jest: " << -b/2*a << endl;
+
     }
-    msc0=(-b)/a;
-    cout << "Miejsce zerowe funkci to: " << msc0;
+
+    else
+    {
+        cout << "Miejsca zerowe funkcji to: " << msc0 << " ," << msc1;
+    }
 
 
 
 
     return 0;
 }
-
